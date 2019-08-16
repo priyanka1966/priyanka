fb05d8738f8c8bc50ad8fd73d34711bfaefa38d0
priyanka1966 <53751071+priyanka1966@users.noreply.github.com>
2019-08-16 19:18:09 +0530

    Add files via upload

---
+++ datatype.java
@@ -0,0 +1,24 @@
+class datatype
+{
+	boolean b;
+		char c;
+		byte a;
+		short s;
+		int i;
+		long p;
+		float f;
+		double d;
+	public static void main(String ar[])
+	{
+		datatype ob=new datatype();
+		
+		System.out.println(ob.b);
+		System.out.println(ob.c);
+		System.out.println(ob.a);
+		System.out.println(ob.s);
+		System.out.println(ob.i);
+		System.out.println(ob.p);
+		System.out.println(ob.f);
+		System.out.println(ob.d);
+	}
+}
