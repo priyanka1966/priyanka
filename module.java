ddbec5c321c43b1027ccd8bd741b50f0eb88d460
priyanka1966 <53751071+priyanka1966@users.noreply.github.com>
2019-08-17 12:34:18 +0530

    Add files via upload

---
+++ module.java
@@ -0,0 +1,11 @@
+class module
+{
+	public static void main(String ar[])
+	{
+		int x=42;
+		double y=42.25;
+		System.out.println("x mod 10 ="+ x%10);
+		System.out.println("ymod 10 ="+ y%10);
+		
+	}
+}
--- unary.java
+++ unary.java
@@ -2,11 +2,18 @@ class unary
 {
 	public static void main(String ar[])
 	{
-		int x=5;
-		System.out.println(x++);
-		System.out.println(++x);
-		System.out.println(x--);
-		System.out.println(--x);
+		int a=1;
+		int b=2;
+		int c,d;
+		c=++b;
+		d=a++;
+		c++;
+		System.out.println("a="+ a);
 		
+		System.out.println("b="+ b);
+		
+		System.out.println("c="+ c);
+		
+		System.out.println("d="+ d);
 	}
 }
